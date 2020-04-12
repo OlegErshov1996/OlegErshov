7a54b2193fb1715f25133a5c369e4aa43ab76943
OlegErshov1996 <oleg_ershov1996@mail.ru>
2020-04-12 16:55:04 +0300

    Рефакторинг раздела Навыки

--- js/slider.js
+++ js/slider.js
@@ -1,30 +1,63 @@
-const sliderContainer = document.querySelector('.slider-container');
-const slideRight = document.querySelector('.right-slide');
-const slideLeft = document.querySelector('.left-slide');
-const upButton = document.querySelector('.up-button');
-const downButton = document.querySelector('.down-button');
-const slidesLength = slideRight.querySelectorAll('div').length;
+/* ================================================== Desktop ==================================================== */
+const sliderContainer_desktop = document.querySelector('.slider-container_desktop');
+const slideRight_desktop = document.querySelector('.right-slide_desktop');
+const slideLeft_desktop = document.querySelector('.left-slide_desktop');
+const upButton_desktop = document.querySelector('.up-button_desktop');
+const downButton_desktop = document.querySelector('.down-button_desktop');
+const slidesLength_desktop = slideRight_desktop.querySelectorAll('.slider-image_desktop').length;
 
-let activeSlideIndex = 0;
+let activeSlideIndex_desktop = 0;
 
-slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+slideLeft_desktop.style.top = `-${(slidesLength_desktop - 1) * 80}vh`;
 
-upButton.addEventListener('click', () => changeSlide('up'));
-downButton.addEventListener('click', () => changeSlide('down'));
+upButton_desktop.addEventListener('click', () => changeSlide_desktop('up'));
+downButton_desktop.addEventListener('click', () => changeSlide_desktop('down'));
 
-const changeSlide = (direction) => {
-    const sliderHeight = sliderContainer.clientHeight;
+const changeSlide_desktop = (direction) => {
+    const sliderHeight_desktop = sliderContainer_desktop.clientHeight;
     if(direction === 'up') {
-        activeSlideIndex++;
-        if(activeSlideIndex > slidesLength - 1) {
-            activeSlideIndex = 0;
+        activeSlideIndex_desktop++;
+        if(activeSlideIndex_desktop > slidesLength_desktop - 1) {
+            activeSlideIndex_desktop = 0;
         }
     } else if (direction === 'down') {
-        activeSlideIndex--;
-        if(activeSlideIndex < 0) {
-            activeSlideIndex = slidesLength - 1;
+        activeSlideIndex_desktop--;
+        if(activeSlideIndex_desktop < 0) {
+            activeSlideIndex_desktop = slidesLength_desktop - 1;
         }
     }
-    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
-    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
+    slideRight_desktop.style.transform = `translateY(-${activeSlideIndex_desktop * sliderHeight_desktop}px)`;
+    slideLeft_desktop.style.transform = `translateY(${activeSlideIndex_desktop * sliderHeight_desktop}px)`;
 }
+
+/* ================================================ Table ======================================================*/
+// const sliderContainer_table = document.querySelector('.slider-container_table');
+// const slideRight_table = document.querySelector('.right-slide_table');
+// const slideLeft_table = document.querySelector('.left-slide_table');
+// const upButton_table = document.querySelector('.up-button_table');
+// const downButton_table = document.querySelector('.down-button_table');
+// const slidesLength_table = slideRight_table.querySelectorAll('.slider-image_table').length;
+//
+// let activeSlideIndex_table = 0;
+//
+// slideLeft_table.style.top = `-${(slidesLength_table - 1) * 50}vh`;
+//
+// upButton_table.addEventListener('click', () => changeSlide_table('up'));
+// downButton_table.addEventListener('click', () => changeSlide_table('down'));
+//
+// const changeSlide_table = (direction) => {
+//     const sliderHeight_table = sliderContainer_table.clientHeight;
+//     if(direction === 'up') {
+//         activeSlideIndex_table++;
+//         if(activeSlideIndex_table > slidesLength_table - 1) {
+//             activeSlideIndex_table = 0;
+//         }
+//     } else if (direction === 'down') {
+//         activeSlideIndex_table--;
+//         if(activeSlideIndex_table < 0) {
+//             activeSlideIndex_table = slidesLength_table - 1;
+//         }
+//     }
+//     slideRight_table.style.transform = `translateY(-${activeSlideIndex_table * sliderHeight_table}px)`;
+//     slideLeft_table.style.transform = `translateY(${activeSlideIndex_table * sliderHeight_table}px)`;
+// }
