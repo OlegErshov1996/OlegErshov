d164061a300b41dea9f925e0f0d42190ea81118b
OlegErshov1996 <50622892+OlegErshov1996@users.noreply.github.com>
2021-06-06 16:19:38 +0300

    Автозакрытие мобильного меню при выборе пункта меню. Ссылки на соцсети

--- dist/js/mobile_menu.js
+++ dist/js/mobile_menu.js
@@ -1,6 +1,6 @@
-const mobileMenuButton = document.querySelector('.mobile_menu_button');
-const mobileMenuItems = document.querySelector('.mobile_menu_items');
-const mobileMenuLink = document.querySelectorAll('.mobile_menu_items .mobile_menu_link');
+//const mobileMenuButton = document.querySelector('.mobile_menu_button');
+//const mobileMenuItems = document.querySelector('.mobile_menu_items');
+//const mobileMenuLink = document.querySelectorAll('.mobile_menu_items .mobile_menu_link');
 
 // function toggleMenu() {
 //     const menuToggle = document.querySelector('.toggle');
@@ -9,36 +9,52 @@ const mobileMenuLink = document.querySelectorAll('.mobile_menu_items .mobile_men
 //     mobile_menu_wrap.classList.toggle('active');
 // }
 
-mobileMenuButton.addEventListener('click', () => {
-    mobileMenuItems.classList.toggle('open_menu');
-    mobileMenuLink.forEach(linkMobile => {
-        linkMobile.classList.toggle('fade');
-    });
-});
+//mobileMenuButton.addEventListener('click', () => {
+//   mobileMenuItems.classList.toggle('open_menu');
+//   mobileMenuLink.forEach(linkMobile => {
+//       linkMobile.classList.toggle('fade');
+//   });
+//});
+
+//mobileMenuItems.addEventListener("click", function (e) {
+//    if (e.target.closest(".mobile_menu_link")) {
+//        mobileMenuItems.classList.toggle("open_menu");
+//        mobileMenuLink.forEach((linkMobile) => {
+//            linkMobile.classList.toggle("fade");
+//        });
+//    }
+//});
 
 /* ========================= Mobile Menu =========================== */
 
 
-document.addEventListener("DOMContentLoaded", function() {
-    let link = document.querySelector('.mobile_menu_button');
-    let menu = document.querySelector('.mobile_menu_wrap');
+// document.addEventListener("DOMContentLoaded", function() {
+//    const link = document.querySelector('.mobile_menu_button');
+//    const menu = document.querySelector('.mobile_menu_wrap');
 
-    link.addEventListener('click', function() {
-        link.classList.toggle('mobile_menu_button_active');
-        menu.classList.toggle('mobile_menu_wrap_active');
-    });
-});
+    // link.addEventListener('click', function() {
+//    link.onclick = function () {
+//        link.classList.toggle('mobile_menu_button_active');
+//        menu.classList.toggle('mobile_menu_wrap_active');
+//    };
+
+//    function toggleMenu() {
+//        link.classList.remove('mobile_menu_button_active');
+//        menu.classList.remove('mobile_menu_wrap_active');
+//    };
+
+
+// });
 
 /* ======================= Remove Mobile Menu ======================= */
 
-const mobileLink = document.querySelectorAll('.mobile_link')
-
-function linkAction(){
-    const mobileMenuWrap = document.getElementById('mobile_menu_wrap')
-    // When we click on each nav__link, we remove the show-menu class
-    mobileMenuWrap.classList.remove('show-menu')
-}
-mobileLink.forEach(n => n.addEventListener('click', linkAction))
+// const mobileLink = document.querySelectorAll('.mobile_link')
+//
+// function linkAction(){
+//     const mobileMenuWrap = document.getElementById('mobile_menu_wrap')
+//     mobileMenuWrap.classList.remove('show-menu')
+// }
+// mobileLink.forEach(n => n.addEventListener('click', linkAction))
 
 // const mobile_menu_items = document.getElementById('mobile_menu_items')
 // const mobile_menu_wrap = document.getElementById('mobile_menu_wrap')
@@ -54,3 +70,29 @@ mobileLink.forEach(n => n.addEventListener('click', linkAction))
 //     mobile_menu_items.classList.toggle('mobile_menu_button_active');
 //     mobile_menu_wrap.classList.toggle('mobile_menu_wrap_active')
 // }
+
+const mobileMenuButton = document.querySelector(".mobile_menu_button");
+const mobileMenuItems = document.querySelector(".mobile_menu_items");
+const mobileMenuLink = document.querySelectorAll(".mobile_menu_items .mobile_menu_link");
+const menu = document.querySelector(".mobile_menu_wrap");
+
+mobileMenuItems.addEventListener("click", function (e) {
+    if (e.target.closest(".mobile_menu_link")) {
+        toggle_mobile();
+    }
+});
+
+mobileMenuButton.onclick = toggle_mobile;
+
+function toggle_mobile() {
+    mobileMenuItems.classList.toggle("open_menu");
+    mobileMenuLink.forEach((linkMobile) => {
+        linkMobile.classList.toggle("fade");
+    });
+    toggleMenu();
+}
+
+function toggleMenu() {
+    mobileMenuButton.classList.toggle("mobile_menu_button_active");
+    menu.classList.toggle("mobile_menu_wrap_active");
+}
